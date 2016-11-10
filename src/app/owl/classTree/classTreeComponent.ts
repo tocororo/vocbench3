ff0116c182d8b8e636f9ed99a5d8f5bee0824e2d
tlorenzetti <tiziano.lorenzetti@gmail.com>
2016-11-10 17:06:13 +0100

    Implemented a workaround in order to solve ST-316

--- src/app/owl/classTree/classTreeComponent.ts
+++ src/app/owl/classTree/classTreeComponent.ts
@@ -115,7 +115,15 @@ export class ClassTreeComponent {
                 var childrenNodeComponent = this.viewChildrenNode.toArray();
                 //open tree from root to node
                 for (var i = 0; i < childrenNodeComponent.length; i++) {//looking for first node (root) to expand
-                    if (childrenNodeComponent[i].node.getURI() == path[0].getURI()) {
+                    if (path[0].getURI() != OWL.thing.getURI() && childrenNodeComponent[i].node.getURI() == OWL.thing.getURI()) {
+                        /* Workaround to resolve an issue:
+                        some classes (e.g. skos:Concept, skos:Collection,...) are visible in class tree of SKOS projects,
+                        but they are not subClassOf owl:Thing, so getPathFromRoot does not return the path up to owl:Thing.
+                        Here I perform a check to skip this scenario. If first node of path is not owl:Thing, I expand owl:Thing
+                        anyway when encountered in this for loop (without splice the first node of the path). */
+                        childrenNodeComponent[i].expandPath(path);
+                        break;
+                    } else if (childrenNodeComponent[i].node.getURI() == path[0].getURI()) {
                         //let the found node expand itself and the remaining path
                         path.splice(0, 1);
                         childrenNodeComponent[i].expandPath(path);
