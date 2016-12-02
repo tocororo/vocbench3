796d01ff1af9fdaaa58536849282fcfc126de8b3
tlorenzetti <tiziano.lorenzetti@gmail.com>
2016-12-02 18:01:59 +0100

    Improvements in project-user binding management

--- src/app/administration/projectsAdministrationComponent.ts
+++ src/app/administration/projectsAdministrationComponent.ts
@@ -8,6 +8,7 @@ import { User } from "../utils/User";
 import { Project } from "../utils/Project";
 import { ProjectServices } from "../services/projectServices";
 import { UserServices } from "../services/userServices";
+import { AdministrationServices } from "../services/administrationServices";
 
 
 @Component({
@@ -17,38 +18,42 @@ import { UserServices } from "../services/userServices";
 })
 export class ProjectsAdministrationComponent {
 
-    private users: User[];
-    private projects: Project[];
+    private usersBound: User[];
+    private projectList: Project[];
 
     private selectedProject: Project;
     private selectedUser: User;
 
-    constructor(private projectService: ProjectServices, private userService: UserServices, private modal: Modal) { }
+    constructor(private projectService: ProjectServices, private userService: UserServices, private adminService: AdministrationServices, 
+        private modal: Modal) { }
 
     ngOnInit() {
         this.projectService.listProjects().subscribe(
             projects => {
-                this.projects = projects;
+                this.projectList = projects;
             }
         );
     }
 
     private selectProject(project: Project) {
-        this.selectedProject = project;
-        this.userService.listUsersBoundToProject(this.selectedProject.getName()).subscribe(
-            users => {
-                this.users = users;
-                this.selectedUser = null;
-            }
-        )
+        if (this.selectedProject != project) {
+            this.selectedProject = project;
+            this.userService.listUsersBoundToProject(this.selectedProject.getName()).subscribe(
+                users => {
+                    this.usersBound = users;
+                    this.selectedUser = null;
+                }
+            )
+        }
     }
 
     private selectUser(user: User) {
         this.selectedUser = user;
+        this.adminService.getProjectUserBinding(this.selectedProject.getName(), this.selectedUser.getEmail()).subscribe();
     }
 
     private addUserToProject() {
-        var modalData = new UserProjBindingModalData("Add user to " + this.selectedProject.getName(), this.users);
+        var modalData = new UserProjBindingModalData("Add user to " + this.selectedProject.getName(), this.usersBound);
         const builder = new BSModalContextBuilder<UserProjBindingModalData>(
             modalData, undefined, UserProjBindingModalData
         );
@@ -59,6 +64,11 @@ export class ProjectsAdministrationComponent {
                     var user: User = data.user;
                     var roles = data.roles;
                     console.log("adding user " + user.getLastName() + " to " + this.selectedProject.getName() + " with roles " + JSON.stringify(roles));
+                    this.adminService.addProjectUserBinding(this.selectedProject.getName(), user.getEmail(), roles).subscribe(
+                        stResp => {
+                            this.usersBound.push(user);
+                        }
+                    )
                 },
                 () => {}
             )
--- src/app/services/administrationServices.ts
+++ src/app/services/administrationServices.ts
@@ -4,9 +4,72 @@ import {HttpManager} from "../utils/HttpManager";
 @Injectable()
 export class AdministrationServices {
 
-    private serviceName = "administration";
+    private serviceName = "Administration";
     private oldTypeService = false;
 
     constructor(private httpMgr: HttpManager) { }
+
+    /**
+     * Adds a role to a user in a project
+     * @param projectName
+     * @param email
+     * @param role
+     */
+    getProjectUserBinding(projectName: string, email: string) {
+        console.log("[AdministrationServices] getProjectUserBinding");
+        var params: any = {
+            projectName: projectName,
+            email: email
+        };
+        return this.httpMgr.doGet(this.serviceName, "getProjectUserBinding", params, this.oldTypeService, true);
+    }
+
+    /**
+     * Assigns a user to a project with the given roles, so creates a new binding
+     * @param projectName
+     * @param email
+     * @param roles
+     */
+    addProjectUserBinding(projectName: string, email: string, roles: string[]) {
+        console.log("[AdministrationServices] addProjectUserBinding");
+        var params: any = {
+            projectName: projectName,
+            email: email,
+            roles: roles
+        };
+        return this.httpMgr.doGet(this.serviceName, "addProjectUserBinding", params, this.oldTypeService, true);
+    }
+
+    /**
+     * Adds a role to a user in a project
+     * @param projectName
+     * @param email
+     * @param role
+     */
+    addRoleToUserInProject(projectName: string, email: string, role: string) {
+        console.log("[AdministrationServices] addRoleToUserInProject");
+        var params: any = {
+            projectName: projectName,
+            email: email,
+            roles: role
+        };
+        return this.httpMgr.doGet(this.serviceName, "addRoleToUserInProject", params, this.oldTypeService, true);
+    }
+
+    /**
+     * Removes a role to a user in a project
+     * @param projectName
+     * @param email
+     * @param role
+     */
+    removeRoleToUserInProject(projectName: string, email: string, role: string) {
+        console.log("[AdministrationServices] removeRoleToUserInProject");
+        var params: any = {
+            projectName: projectName,
+            email: email,
+            roles: role
+        };
+        return this.httpMgr.doGet(this.serviceName, "removeRoleToUserInProject", params, this.oldTypeService, true);
+    }
 
 }
