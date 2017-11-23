tocororo/vocbench3

commit 870ac76b6757d47436ff41ee4f23e6b2e9e71ae3

Changed creation services invocation; Fixed minor bug that didn't save changes to search settings.

src/app/widget/modal/creationModal/newResourceModal/newConceptCfModal.ts
@@ -6,6 +6,7 @@ import { CustomFormsServices } from "../../../../services/customFormsServices"
 import { BasicModalServices } from "../../basicModal/basicModalServices"
 import { BrowsingModalServices } from "../../browsingModal/browsingModalServices"
 import { ARTLiteral, ARTURIResource } from "../../../../models/ARTResources"
+import { CustomFormValue } from "../../../../models/CustomForms"
 import { SKOS } from "../../../../models/Vocabulary"
 
 export class NewConceptCfModalData extends BSModalContext {
@@ -80,13 +81,12 @@ export class NewConceptCfModal extends AbstractCustomConstructorModal implements
 
         var entryMap: any = this.collectCustomFormData();
 
-        var returnedData: { uriResource: ARTURIResource, label: ARTLiteral, cls: ARTURIResource, schemes: ARTURIResource[], cfId: string, cfValueMap: any} = {
+        var returnedData: { uriResource: ARTURIResource, label: ARTLiteral, cls: ARTURIResource, schemes: ARTURIResource[], cfValue: CustomFormValue } = {
             uriResource: null,
             label: new ARTLiteral(this.label, null, this.lang),
             cls: null,
             schemes: this.schemes,
-            cfId: this.customFormId,
-            cfValueMap: entryMap
+            cfValue: new CustomFormValue(this.customFormId, entryMap)
         }
         //Set URI only if localName is not empty
         if (this.uri != null && this.uri.trim() != "") {

src/app/widget/modal/creationModal/newResourceModal/newConceptFromLabelModal.ts
@@ -6,7 +6,7 @@ import { CustomFormsServices } from "../../../../services/customFormsServices";
 import { SkosServices } from "../../../../services/skosServices";
 import { BasicModalServices } from "../../basicModal/basicModalServices";
 import { BrowsingModalServices } from "../../browsingModal/browsingModalServices";
-import { CustomForm, FormField } from "../../../../models/CustomForms";
+import { CustomForm, FormField, CustomFormValue } from "../../../../models/CustomForms";
 import { ARTLiteral, ARTURIResource, ARTResource } from "../../../../models/ARTResources";
 import { VBProperties } from "../../../../utils/VBProperties";
 
@@ -155,13 +155,12 @@ export class NewConceptFromLabelModal extends AbstractCustomConstructorModal imp
 
         var entryMap: any = this.collectCustomFormData();
 
-        var returnedData: { uriResource: ARTURIResource, broader: ARTURIResource, cls: ARTURIResource, schemes: ARTURIResource[], cfId: string, cfValueMap: any} = {
+        var returnedData: { uriResource: ARTURIResource, broader: ARTURIResource, cls: ARTURIResource, schemes: ARTURIResource[], cfValue: CustomFormValue } = {
             uriResource: null,
             broader: null,
             cls: null,
             schemes: this.schemes,
-            cfId: this.customFormId,
-            cfValueMap: entryMap
+            cfValue: new CustomFormValue(this.customFormId, entryMap)
         }
         //Set URI only if not empty
         if (this.uri != null && this.uri.trim() != "") {

src/app/widget/modal/creationModal/newResourceModal/newResourceCfModal.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { BSModalContext } from 'ngx-modialog/plugins/bootstrap';
 import { DialogRef, ModalComponent } from "ngx-modialog";
-import { AbstractCustomConstructorModal } from "./abstractCustomConstructorModal"
-import { CustomFormsServices } from "../../../../services/customFormsServices"
-import { BrowsingModalServices } from "../../browsingModal/browsingModalServices"
-import { BasicModalServices } from "../../basicModal/basicModalServices"
-import { ARTLiteral, ARTURIResource } from "../../../../models/ARTResources"
+import { AbstractCustomConstructorModal } from "./abstractCustomConstructorModal";
+import { CustomFormsServices } from "../../../../services/customFormsServices";
+import { BrowsingModalServices } from "../../browsingModal/browsingModalServices";
+import { BasicModalServices } from "../../basicModal/basicModalServices";
+import { ARTLiteral, ARTURIResource } from "../../../../models/ARTResources";
+import { CustomFormValue } from "../../../../models/CustomForms";
 
 export class NewResourceCfModalData extends BSModalContext {
     constructor(
@@ -52,11 +53,10 @@ export class NewResourceCfModal extends AbstractCustomConstructorModal implement
 
         var entryMap: any = this.collectCustomFormData();
 
-        var returnedData: { uriResource: ARTURIResource, cls: ARTURIResource, cfId: string, cfValueMap: any} = {
+        var returnedData: { uriResource: ARTURIResource, cls: ARTURIResource, cfValue: CustomFormValue } = {
             uriResource: new ARTURIResource(this.uri),
             cls: this.resourceClass,
-            cfId: this.customFormId,
-            cfValueMap: entryMap
+            cfValue: new CustomFormValue(this.customFormId, entryMap)
         }
         this.dialog.close(returnedData);
     }

src/app/widget/modal/creationModal/newResourceModal/newSkosResourceCfModal.ts
@@ -1,12 +1,13 @@
 import { Component, ViewChild, ElementRef } from "@angular/core";
 import { BSModalContext } from 'ngx-modialog/plugins/bootstrap';
 import { DialogRef, ModalComponent } from "ngx-modialog";
-import { AbstractCustomConstructorModal } from "./abstractCustomConstructorModal"
-import { CustomFormsServices } from "../../../../services/customFormsServices"
-import { BasicModalServices } from "../../basicModal/basicModalServices"
-import { BrowsingModalServices } from "../../browsingModal/browsingModalServices"
-import { ARTLiteral, ARTURIResource } from "../../../../models/ARTResources"
-import { SKOS } from "../../../../models/Vocabulary"
+import { AbstractCustomConstructorModal } from "./abstractCustomConstructorModal";
+import { CustomFormsServices } from "../../../../services/customFormsServices";
+import { BasicModalServices } from "../../basicModal/basicModalServices";
+import { BrowsingModalServices } from "../../browsingModal/browsingModalServices";
+import { ARTLiteral, ARTURIResource } from "../../../../models/ARTResources";
+import { CustomFormValue } from "../../../../models/CustomForms";
+import { SKOS } from "../../../../models/Vocabulary";
 
 export class NewSkosResourceCfModalData extends BSModalContext {
     constructor(
@@ -74,12 +75,11 @@ export class NewSkosResourceCfModal extends AbstractCustomConstructorModal imple
 
         var entryMap: any = this.collectCustomFormData();
 
-        var returnedData: { uriResource: ARTURIResource, label: ARTLiteral, cls: ARTURIResource, cfId: string, cfValueMap: any} = {
+        var returnedData: { uriResource: ARTURIResource, label: ARTLiteral, cls: ARTURIResource, cfValue: CustomFormValue } = {
             uriResource: null,
             label: new ARTLiteral(this.label, null, this.lang),
             cls: null,
-            cfId: this.customFormId,
-            cfValueMap: entryMap
+            cfValue: new CustomFormValue(this.customFormId, entryMap)
         }
         //Set URI only if localName is not empty
         if (this.uri != null && this.uri.trim() != "") {

src/app/icv/noTopConceptScheme/noTopConceptSchemeComponent.ts
@@ -59,7 +59,7 @@ export class NoTopConceptSchemeComponent {
     createTopConcept(scheme: ARTURIResource) {
         this.creationModals.newConceptCf("Create new skos:Concept", null, true).then(
             (data: any) => {
-                this.skosService.createConcept(data.label, data.schemes, data.uriResource, null, data.cls, data.cfId, data.cfValueMap).subscribe(
+                this.skosService.createConcept(data.label, data.schemes, data.uriResource, null, data.cls, data.cfValue).subscribe(
                     stResp => {
                         this.runIcv();
                     },
@@ -67,7 +67,7 @@ export class NoTopConceptSchemeComponent {
                         if (err.name.endsWith('PrefAltLabelClashException')) {
                             this.basicModals.confirm("Warning", err.message + " Do you want to force the creation?", "warning").then(
                                 confirm => {
-                                    this.skosService.createConcept(data.label, data.schemes, data.uriResource, null, data.cls, data.cfId, data.cfValueMap, false).subscribe(
+                                    this.skosService.createConcept(data.label, data.schemes, data.uriResource, null, data.cls, data.cfValue, false).subscribe(
                                         stResp => {
                                             this.runIcv();
                                         },

src/app/resourceView/resViewResources/editableResourceComponent.ts
@@ -328,7 +328,7 @@ export class EditableResourceComponent {
 			data => {
 				let oldConcept: ARTURIResource = <ARTURIResource>this.subject;
 				this.refactorService.spawnNewConceptFromLabel(<ARTResource>this.resource, data.schemes, oldConcept,
-					data.uriResource, data.broader, data.cfId, data.cfValueMap).subscribe(
+					data.uriResource, data.broader, data.cfValue).subscribe(
 					stResp => {
 						this.update.emit();
 					}

src/app/resourceView/resourceViewCtxMenu.ts
@@ -56,7 +56,7 @@ export class ResourceViewContextMenu {
                 //from the resView of the xLabel I don't know the concept to which it belongs, 
                 //so oldConcept in spawnNewConceptFromLabel request is null and lets the server find the oldConcept
                 this.refactorService.spawnNewConceptFromLabel(this.resource, data.schemes, null,
-                    data.uriResource, data.broader, data.cfId, data.cfValueMap).subscribe(
+                    data.uriResource, data.broader, data.cfValue).subscribe(
                     stResp => {
                         this.update.emit();
                     }

src/app/services/classesServices.ts
@@ -4,6 +4,7 @@ import { HttpManager } from "../utils/HttpManager";
 import { Deserializer } from "../utils/Deserializer";
 import { VBEventHandler } from "../utils/VBEventHandler";
 import { ARTResource, ARTURIResource, ARTNode, ARTBNode, ResAttribute } from "../models/ARTResources";
+import { CustomFormValue } from "../models/CustomForms";
 import { ResourcesServices } from "./resourcesServices"
 
 @Injectable()
@@ -76,19 +77,16 @@ export class ClassesServices {
      * Creates a new class
      * @param newClass 
      * @param superClass 
-     * @param customFormId id of the custom form that set additional info to the concept
-     * @param userPromptMap json map object of key - value of the custom form
+     * @param customFormValue custom form that set additional info to the concept
      */
-    createClass(newClass: ARTURIResource, superClass: ARTURIResource,
-        customFormId?: string, userPromptMap?: any) {
+    createClass(newClass: ARTURIResource, superClass: ARTURIResource, customFormValue?: CustomFormValue) {
         console.log("[ClassesServices] createClass");
         var params: any = {
             newClass: newClass,
             superClass: superClass
         };
-        if (customFormId != null && userPromptMap != null) {
-            params.customFormId = customFormId;
-            params.userPromptMap = JSON.stringify(userPromptMap);
+        if (customFormValue != null) {
+            params.customFormValue = customFormValue;
         }
         return this.httpMgr.doPost(this.serviceName, "createClass", params, true).map(
             stResp => {
@@ -131,19 +129,16 @@ export class ClassesServices {
      * Creates a new instance for the given class
      * @param newInstance 
      * @param cls 
-     * @param customFormId id of the custom form that set additional info to the concept
-     * @param userPromptMap json map object of key - value of the custom form
+     * @param customFormValue custom form that set additional info to the concept
      */
-    createInstance(newInstance: ARTURIResource, cls: ARTURIResource,
-        customFormId?: string, userPromptMap?: any) {
+    createInstance(newInstance: ARTURIResource, cls: ARTURIResource, customFormValue?: CustomFormValue) {
         console.log("[ClassesServices] createInstance");
         var params: any = {
             newInstance: newInstance,
             cls: cls
         };
-        if (customFormId != null && userPromptMap != null) {
-            params.customFormId = customFormId;
-            params.userPromptMap = JSON.stringify(userPromptMap);
+        if (customFormValue != null) {
+            params.customFormValue = customFormValue;
         }
         return this.httpMgr.doPost(this.serviceName, "createInstance", params, true).map(
             stResp => {

commit a28b37ad67fb80d440ace874ac31279fedccb22b

Fixed bug introduced in commit 870ac76

src/app/widget/modal/creationModal/newResourceModal/newConceptCfModal.ts
@@ -86,7 +86,7 @@ export class NewConceptCfModal extends AbstractCustomConstructorModal implements
             label: new ARTLiteral(this.label, null, this.lang),
             cls: null,
             schemes: this.schemes,
-            cfValue: new CustomFormValue(this.customFormId, entryMap)
+            cfValue: null
         }
         //Set URI only if localName is not empty
         if (this.uri != null && this.uri.trim() != "") {
@@ -96,6 +96,11 @@ export class NewConceptCfModal extends AbstractCustomConstructorModal implements
         if (this.resourceClass.getURI() != SKOS.concept.getURI()) {
             returnedData.cls = this.resourceClass;
         }
+        //set cfValue only if not null
+        if (this.customFormId != null && entryMap != null) {
+            returnedData.cfValue = new CustomFormValue(this.customFormId, entryMap);
+        }
+        
         this.dialog.close(returnedData);
     }
 

src/app/widget/modal/creationModal/newResourceModal/newConceptFromLabelModal.ts
@@ -160,7 +160,7 @@ export class NewConceptFromLabelModal extends AbstractCustomConstructorModal imp
             broader: null,
             cls: null,
             schemes: this.schemes,
-            cfValue: new CustomFormValue(this.customFormId, entryMap)
+            cfValue: null
         }
         //Set URI only if not empty
         if (this.uri != null && this.uri.trim() != "") {
@@ -176,6 +176,10 @@ export class NewConceptFromLabelModal extends AbstractCustomConstructorModal imp
         //set class only if not the default
         if (this.resourceClass.getURI() != this.context.cls.getURI()) {
             returnedData.cls = this.resourceClass;
+        }
+        //set cfValue only if not null
+        if (this.customFormId != null && entryMap != null) {
+            returnedData.cfValue = new CustomFormValue(this.customFormId, entryMap);
         }
         this.dialog.close(returnedData);
     }

src/app/widget/modal/creationModal/newResourceModal/newResourceCfModal.ts
@@ -56,7 +56,11 @@ export class NewResourceCfModal extends AbstractCustomConstructorModal implement
         var returnedData: { uriResource: ARTURIResource, cls: ARTURIResource, cfValue: CustomFormValue } = {
             uriResource: new ARTURIResource(this.uri),
             cls: this.resourceClass,
-            cfValue: new CustomFormValue(this.customFormId, entryMap)
+            cfValue: null
+        }
+        //set cfValue only if not null
+        if (this.customFormId != null && entryMap != null) {
+            returnedData.cfValue = new CustomFormValue(this.customFormId, entryMap);
         }
         this.dialog.close(returnedData);
     }

src/app/widget/modal/creationModal/newResourceModal/newSkosResourceCfModal.ts
@@ -79,7 +79,7 @@ export class NewSkosResourceCfModal extends AbstractCustomConstructorModal imple
             uriResource: null,
             label: new ARTLiteral(this.label, null, this.lang),
             cls: null,
-            cfValue: new CustomFormValue(this.customFormId, entryMap)
+            cfValue: null
         }
         //Set URI only if localName is not empty
         if (this.uri != null && this.uri.trim() != "") {
@@ -88,6 +88,10 @@ export class NewSkosResourceCfModal extends AbstractCustomConstructorModal imple
         //set class only if not the default
         if (this.resourceClass.getURI() != this.context.cls.getURI()) {
             returnedData.cls = this.resourceClass;
+        }
+        //set cfValue only if not null
+        if (this.customFormId != null && entryMap != null) {
+            returnedData.cfValue = new CustomFormValue(this.customFormId, entryMap);
         }
         this.dialog.close(returnedData);
     }