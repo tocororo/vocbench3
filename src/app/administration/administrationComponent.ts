fe8b8489256cfae9b8f05c161bd8955103d359ec
tlorenzetti <tiziano.lorenzetti@gmail.com>
2019-07-01 16:36:47 +0200

    Minor changes

--- src/app/administration/administrationComponent.ts
+++ src/app/administration/administrationComponent.ts
@@ -13,23 +13,6 @@ export class AdministrationComponent {
 
     constructor(private router: Router) { }
 
-    ngOnInit() {
-        if (this.isAdmin()) { //user admin => send to Users page
-            this.router.navigate(["/Administration/Users"]);
-        } else if (VBContext.getWorkingProject() != undefined) {
-            if (this.isProjManagementAuthorized()) {
-                this.router.navigate(["/Administration/Projects"]);
-            } else if (this.isRoleManagementAuthorized()) {
-                this.router.navigate(["/Administration/Roles"]);
-            } else {
-                this.router.navigate(["/Home"]);    
-            }
-        } else {
-            //if user is neither admin or PM redirect to home page
-            this.router.navigate(["/Home"]);
-        }
-    }
-
     private isAdmin(): boolean {
         var user = VBContext.getLoggedUser();
         if (user != null) {
