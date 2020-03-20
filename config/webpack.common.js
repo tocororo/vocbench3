30c4e2b4c004e89c205ecd0c167c89feb94daf5d
tlorenzetti <tiziano.lorenzetti@gmail.com>
2020-03-20 10:49:45 +0100

    Removed a duplicated tsconfig.json

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
                 loaders: [
                     {
                         loader: 'awesome-typescript-loader',
-                        options: { configFileName: helpers.root('src', 'tsconfig.json') }
+                        options: { configFileName: helpers.root('.', 'tsconfig.json') }
                     } , 'angular2-template-loader'
                 ]
             },
