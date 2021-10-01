8f64b184ab67b10debf3e663535e0ad56411125e
Tiziano Lorenzetti <tiziano.lorenzetti@gmail.com>
2021-10-01 15:29:15 +0200

    Added french translation file

--- src/app/appComponent.ts
+++ src/app/appComponent.ts
@@ -30,7 +30,7 @@ export class AppComponent {
         });
 
         //set the available factory-provided l10n languages
-        translate.addLangs(['de', 'en', 'it']);
+        translate.addLangs(['de', 'en', 'fr', 'it']);
         //add additional supported l10n languages
         let additionalLangs: string[] = window['additional_l10n_langs'];
         if (additionalLangs && additionalLangs.length > 0) {
